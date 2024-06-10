670b57385b06e67f10ceb12cd565f915e82a630c
TegestBogale <tegest.g.bogale@gmail.com>
2024-06-10 10:05:30 +0200

    Added lifecycle service dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,5 +77,7 @@ dependencies {
     implementation(libs.androidx.core.ktx.v160)
     implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.android.beacon.library)
+    implementation(libs.androidx.lifecycle.service)
+
 
 }
